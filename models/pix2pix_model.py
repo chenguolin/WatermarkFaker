bc0fe6cea5182a949aef4c9efc85018a53065612
SleepSupreme <linchenguo0622@gmail.com>
2020-07-11 12:34:09 +0800

    modify pix2pix_model to visualize fake watermark

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -1,8 +1,8 @@
 import torch
 from . import networks
 from watermarks import lsb
+from utils.util import tensor2im
 from .base_model import BaseModel
-
 
 class Pix2PixModel(BaseModel):
     """ This class implements the pix2pix model, for learning a mapping from input images to output images given paired data.
@@ -47,7 +47,7 @@ class Pix2PixModel(BaseModel):
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
         self.loss_names = ['G_GAN', 'G_L1', 'D_real', 'D_fake']
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
-        self.visual_names = ['real_A', 'fake_B', 'watermark']
+        self.visual_names = ['real_A', 'fake_B', 'fake_watermark']
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
         if self.isTrain:
             self.model_names = ['G', 'D']
@@ -82,12 +82,12 @@ class Pix2PixModel(BaseModel):
         AtoB = self.opt.direction == 'AtoB'
         self.real_A = Input['A' if AtoB else 'B'].to(self.device)
         self.real_B = Input['B' if AtoB else 'A'].to(self.device)
-        self.watermark = lsb.LSB().extract(self.real_B)
         self.image_paths = Input['A_paths' if AtoB else 'B_paths']
 
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
         self.fake_B = self.netG(self.real_A)  # G(A)
+        self.fake_watermark = lsb.LSB().extract(tensor2im(self.fake_B))
 
     def backward_D(self):
         """Calculate GAN loss for the discriminator"""
