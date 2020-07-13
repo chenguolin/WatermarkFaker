810a1b39668dbf2bca71fdb9d9df9657b3bcc05b
SleepSupreme <linchenguo0622@gmail.com>
2020-07-14 00:02:16 +0800

    modify bash scripts for pix2pixbits model

--- scripts/test_pix2pix.sh
+++ scripts/test_pix2pix.sh
@@ -1,2 +1,2 @@
 set -ex
-python test.py --dataroot ../datasets/lsb --name lsb_pix2pix --model pix2pix --netG unet_256
+python test.py --dataroot ../datasets/lsb --name lsb_pix2pixbits --model pix2pixbits --netG unet_256
