d5b1ac2353198679be64a7bca8bc43ae18cd309c
SleepSupreme <linchenguo0622@gmail.com>
2020-07-27 16:33:34 +0800

    add ssim loss

--- models/novel_model.py
+++ models/novel_model.py
@@ -26,14 +26,13 @@ class NovelModel(BaseModel):
         Returns:
             the modified parser.
 
-        For pix2pix, authors do not use image buffer
-        The training objective is: GAN Loss + lambda_L1 * ||G(A)-B||_1
+        The training objective is: GAN Loss + lambda_L1 * ||G(A)-B||_1 + lambda_SSIM * (1 - SSIM(G(A), B))
         By default, they use vanilla GAN loss, UNet with batchnorm, and aligned datasets.
         """
         # changing the default values to match the pix2pix paper (https://phillipi.github.io/pix2pix/)
         if is_train:
             parser.add_argument('--lambda_L1', type=float, default=100.0, help='weight for L1 loss')
-            parser.add_argument('--lambda_SSIM', type=float, default=100.0, help='weight for SSIM loss')
+            parser.add_argument('--lambda_SSIM', type=float, default=100.0, help='weight for SSIM loss')  #1 SSIM Loss
         return parser
 
     def __init__(self, opt):
@@ -44,7 +43,7 @@ class NovelModel(BaseModel):
         """
         BaseModel.__init__(self, opt)
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
-        self.loss_names = ['G_GAN', 'G_L1', 'D_real', 'D_fake']
+        self.loss_names = ['G_GAN', 'G_L1', 'G_SSIM', 'D_real', 'D_fake']  #1 SSIM Loss
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
         self.visual_names = ['real_B_img', 'fake_B_img', 'real_watermark', 'fake_watermark']
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
@@ -67,7 +66,7 @@ class NovelModel(BaseModel):
             # define loss functions
             self.criterionGAN = networks.GANLoss(opt.gan_mode).to(self.device)
             self.criterionL1 = torch.nn.L1Loss()
-            self.criterionSSIM = SSIM()
+            self.criterionSSIM = SSIM()  #1 SSIM Loss
             # initialize optimizers; schedulers will be automatically created by function <BaseModel.setup>.
             self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
             self.optimizer_D = torch.optim.Adam(self.netD.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
@@ -95,21 +94,29 @@ class NovelModel(BaseModel):
             self.real_B_img = bits2im(self.real_B)
         else:
             self.real_B_img = self.real_B.detach()
-        
-        self.real_watermark = lsb.LSB().extract(tensor2im(self.real_B_img))
-        # self.real_watermark = rlsb.RobustLSB().extract(tensor2im(self.real_B_img), tensor2im(self.real_A_img))
+
+        if 'lsb' == self.opt.watermark:
+            self.real_watermark = lsb.LSB().extract(tensor2im(self.real_B_img))
+        elif 'rlsb' == self.opt.watermark:
+            self.real_watermark = rlsb.RobustLSB().extract(tensor2im(self.real_B_img), tensor2im(self.real_A_img))
+        else:
+            raise NotImplementedError("Please choose implemented watermark algorithms. [lsb | rlsb | dct]")
         self.image_paths = Input['A_paths' if AtoB else 'B_paths']
 
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>"""
-        self.fake_B = self.real_A + self.netG(self.real_A)
+        self.fake_B = self.netG(self.real_A)
         if self.opt.expand_bits:
             self.fake_B_img = bits2im(self.fake_B)
         else:
             self.fake_B_img = self.fake_B.detach()
         
-        self.fake_watermark = lsb.LSB().extract(tensor2im(self.fake_B_img))
-        # self.fake_watermark = rlsb.RobustLSB().extract(tensor2im(self.fake_B_img), tensor2im(self.real_A_img))
+        if 'lsb' == self.opt.watermark:
+            self.fake_watermark = lsb.LSB().extract(tensor2im(self.fake_B_img))
+        elif 'rlsb' == self.opt.watermark:
+            self.fake_watermark = rlsb.RobustLSB().extract(tensor2im(self.fake_B_img), tensor2im(self.real_A_img))
+        else:             
+            raise NotImplementedError("Please choose implemented watermark algorithms. [lsb | rlsb | dct]")
 
     def backward_D(self):
         """Calculate GAN loss for the discriminator"""
@@ -133,9 +140,9 @@ class NovelModel(BaseModel):
         self.loss_G_GAN = self.criterionGAN(pred_fake, True)
         # Second, G(A) = B
         self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
-        self.loss_G_SSIM = self.criterionSSIM(self.fake_B, self.real_B) * self.opt.lambda_SSIM
+        self.loss_G_SSIM = (1 - self.criterionSSIM(im2tensor(self.fake_B_img), im2tensor(self.real_B_img))) * self.opt.lambda_SSIM  #1 SSIM Loss
         # combine loss and calculate gradients
-        self.loss_G = self.loss_G_GAN + self.loss_G_L1
+        self.loss_G = self.loss_G_GAN + self.loss_G_L1 + self.loss_G_SSIM  #1 SSIM Loss
         self.loss_G.backward()
 
     def optimize_parameters(self):
