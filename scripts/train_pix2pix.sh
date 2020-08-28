2493d782ea94d94f24eb2e0dc6c484465c2736e5
SleepSupreme <linchenguo0622@gmail.com>
2020-08-28 15:46:54 +0800

    remove cyclegan scripts

--- scripts/train_pix2pix.sh
+++ scripts/train_pix2pix.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot ../datasets/lsb --name lsb_pix2pix-bits --model pix2pix --netG unet_256 --netD basic --max_dataset_size 12000 --norm batch --n_epochs 1 --n_epochs_decay 0 --gan_mode vanilla --pool_size 0 --display_freq 100 --expand_bits
+python train.py --dataroot ../datasets/lsb --name pix2pix-expand-lsb --model pix2pix --netG unet_256 --netD basic --max_dataset_size 12288 --norm batch --n_epochs 10 --n_epochs_decay 0 --gan_mode vanilla --pool_size 0 --display_freq 100 --expand_bits --watermark lsb --save_epoch_freq 1
