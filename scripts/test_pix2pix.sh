c54660904d19fb93fecbbb32e85822d796d44293
SleepSupreme <linchenguo0622@gmail.com>
2020-10-21 21:13:20 +0800

    modify scripts

--- scripts/test_pix2pix.sh
+++ scripts/test_pix2pix.sh
@@ -1,2 +1,2 @@
 set -ex
-python test.py --dataroot ../datasets/lsb --results_dir ./results/ --name pix2pix-expand-lsb --model pix2pix --netG unet_256 --num_test 50 --phase test --eval --expand_bits --epoch latest --watermark lsb
+python test.py --dataroot ../datasets/lsb --results_dir ./results/ --name lsb-watermark --model pix2pix --netG unet_256 --num_test 50 --phase test --eval --expand_bits --epoch latest
--- scripts/train_pix2pix.sh
+++ scripts/train_pix2pix.sh
@@ -1,2 +1,2 @@
 set -ex
-python train.py --dataroot ../datasets/lsb --name pix2pix-expand-lsb --model pix2pix --netG unet_256 --netD basic --max_dataset_size 12288 --norm batch --n_epochs 10 --n_epochs_decay 0 --gan_mode vanilla --pool_size 0 --display_freq 100 --expand_bits --watermark lsb --save_epoch_freq 1
+python train.py --dataroot ../datasets/lsb --name lsb-watermark --model pix2pix --netG unet_256 --netD basic --norm batch --n_epochs 10 --n_epochs_decay 0 --gan_mode lsgan --lambda_L1 10.0 --display_freq 100 --expand_bits --input_nc 3 --output_nc 3
